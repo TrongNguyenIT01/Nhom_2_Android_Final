c13c433834e5f17481f37b8abd26533854702b07
Trong_Nguyen <lamnguyenkietco@gmail.com>
2026-05-13 00:44:03 +0700

    Xin Chào

---
+++ app/src/main/java/com/example/nhom_2_android_final/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.nhom_2_android_final;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
